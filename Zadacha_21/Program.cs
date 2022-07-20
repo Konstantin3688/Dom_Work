21cc5b8219b31f6f9c4da491067dacf2ddc2ac77
Konstantin3688 <okatovka3688@gmail.com>
2022-07-20 17:36:18 +0300

    Cdelal

---
+++ Zadacha_21/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.Write("Enter x ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Enter y ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Enter z ");
+int z1 = int.Parse(Console.ReadLine());
+Console.Write("Enter x2 ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Enter y2 ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("Enter z2 ");
+int z2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Длина отрезка равна... " + GetLength(x1, y1, z1, x2, y2, z2));
+double GetLength(int x, int y, int z, int x2, int y2, int z2)
+{
+    double Distance = Math.Sqrt
+    (
+        (x2 - x) * (x2 - x) + (y2 - y) * (y2 - y) + (z2 - z) * (z2 - z)
+    );
+
+    return Distance;
+}
---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.WriteLine("Введите число (N)");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("таблица кубов чисел от 1 до (N)..");
+
+for( int i=1; i<number+1; i++)
+{
+    int TableCubes = i*i*i;
+    Console.Write(" " + TableCubes);
+}
