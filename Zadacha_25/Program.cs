1a8fd9ad9411a8308501c2868bdf3aebba0a3f2e
Konstantin3688 <okatovka3688@gmail.com>
2022-08-05 19:02:50 +0300

    add zadacha25

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 25: 
+Напишите цикл, 
+который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+пример:
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+
+Console.WriteLine("Введите первое число: ");
+int firstNumber_A = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число: ");
+int secondNumber_B = int.Parse(Console.ReadLine());
+
+int sqrt = 1;
+
+for (int i=0; i<secondNumber_B; i++)
+{
+sqrt = sqrt * firstNumber_A;
+}
+
+Console.WriteLine("Первое число возведено в натуральную степень второго числа: ");
+Console.WriteLine(sqrt);
