50bf64bf0062177dd77de093de03ccb7f0685891
Konstantin3688 <okatovka3688@gmail.com>
2022-08-05 19:03:43 +0300

    add zadacha27

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. пример:
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+int i = 0;
+
+while (number != 0)
+{
+    i = number % 10;
+    sum = sum + i;
+    number = number / 10;
+}
+Console.WriteLine($"сумма цифр введеного числа равна:\n{sum}");
+
